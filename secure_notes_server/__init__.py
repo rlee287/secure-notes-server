b716bf71cbb824e632841b563a10229fbf4af02b
rlee287 <rlee287@yahoo.com>
2019-05-19 21:56:18 -0700

    Update config spec to possibly read Flask secret key from config file

--- secure_notes_server/__init__.py
+++ secure_notes_server/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 import configparser
+import os
 
 appconfig=configparser.ConfigParser()
 appconfig.read("serverconfig.cfg")
@@ -15,6 +16,11 @@ if "url" not in appconfig["MongoDB"]:
 app=Flask(__name__)
 app.config["DEBUG"]=True
 app.config["MONGO_URI"]=appconfig["MongoDB"]["url"]
+if "Flask" in appconfig and "secret_key" in appconfig["Flask"]:
+    app.config["SECRET_KEY"]=appconfig["Flask"]["secret_key"]
+else:
+    app.config["SECRET_KEY"]=os.urandom(32)
+
 mongo=PyMongo(app)
 
 from secure_notes_server import routes

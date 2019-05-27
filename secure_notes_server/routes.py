e6b2c2b15de6d762bc8d06774549f7e42971fdd0
rlee287 <rlee287@yahoo.com>
2019-05-26 22:54:56 -0700

    Implement C, R, and D of CRUD operations
    TODO: optimistic locking for update functionality

--- secure_notes_server/routes.py
+++ secure_notes_server/routes.py
@@ -1,9 +1,12 @@
 from secure_notes_server import app, mongo
 from .auth import generate_token, validate_token, validate_password, compute_password_hash
 from .auth import basic_auth, token_auth
+from . import utils
 
 from flask import request, abort, jsonify, g
+import bson
 
+from datetime import datetime
 import html
 import string
 
@@ -68,7 +71,87 @@ def remove_user(user=None):
     # TODO: handle notes
     return '',204
 
+# Note CRUD operations
+
+#Creation
+@app.route("/<user>/notes", methods=["POST"])
+@token_auth.login_required
+def create_note(user):
+    if user!=g.username:
+        abort(400)
+    jsonobj=request.json
+    if jsonobj is None:
+        abort(400)
+    if "title" not in jsonobj and "storage_format" not in jsonobj:
+        abort(400)
+    # FINISH
+    insert_document={
+        "title":jsonobj["title"].encode("utf-8"),
+        "userlist":[g.username],
+        "modified":datetime.utcnow(),
+        "text":bytes(),
+        "storage_format":jsonobj["storage_format"]
+    }
+    #TODO: validator fields in header
+    insert_result=mongo.db.notes.insert_one(insert_document)
+    return jsonify({"id":str(insert_result.inserted_id)}), 201
+
+#Retrieval (individual, list)
+@app.route("/<user>/notes/<id_>")
+@token_auth.login_required
+def retrieve_note(user,id_):
+    if user!=g.username:
+        abort(400)
+    try:
+        id_obj=bson.objectid.ObjectId(id_)
+        if not bson.objectid.ObjectId.is_valid(id_):
+            raise bson.errors.InvalidId()
+    except bson.errors.InvalidId:
+        abort(400)
+    noteobj=mongo.db.notes.find_one(id_obj)
+    if noteobj is None:
+        abort(404)
+    if g.username not in noteobj["userlist"]:
+        abort(403)
+    return jsonify(utils.sanitize_for_json(noteobj))
+
+@app.route("/<user>/notes")
+@token_auth.login_required
+def get_note_list(user):
+    if user!=g.username:
+        abort(400)
+    retlist=list()
+    cursor=mongo.db.notes.find({"userlist":{"$all":[user]}})
+    for doc in cursor:
+        retlist.append(str(doc["_id"]))
+    return html.escape(str(retlist))
+
+#Updates
+#TODO
+
+#Deletion
+@app.route("/<user>/notes/<id_>", methods=["DELETE"])
+@token_auth.login_required
+def delete_note(user, id_):
+    if user!=g.username:
+        abort(400)
+    try:
+        id_obj=bson.objectid.ObjectId(id_)
+        if not bson.objectid.ObjectId.is_valid(id_):
+            raise bson.errors.InvalidId()
+    except bson.errors.InvalidId:
+        abort(400)
+    noteobj=mongo.db.notes.find_one(id_obj)
+    if noteobj is None:
+        abort(404)
+    if g.username not in noteobj["userlist"]:
+        abort(403)
+    # Document guaranteed to exist by previous find_one call
+    mongo.db.notes.delete_one({"_id":id_obj})
+    return '', 204
+
 # Development test endpoints
+
 @app.route("/tokentest")
 @token_auth.login_required
 def test_token():

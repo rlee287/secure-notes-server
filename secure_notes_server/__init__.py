2326b05c0f460b579d1458d7feac3afb619d045e
rlee287 <rlee287@yahoo.com>
2019-05-28 13:40:50 -0700

    Make flask secret key config value mandatory
    Needs to be persistent across restarts for ETags and related stuff

--- secure_notes_server/__init__.py
+++ secure_notes_server/__init__.py
@@ -10,17 +10,16 @@ from . import utils
 appconfig=configparser.ConfigParser()
 appconfig.read("serverconfig.cfg")
 
-if "MongoDB" not in appconfig:
-    raise ValueError("Config file must contain a [MongoDB] section")
-
-if "url" not in appconfig["MongoDB"]:
-    raise ValueError("[MongoDB] config section must have a url value")
+if not utils.check_if_exists(appconfig, "MongoDB", "url"):
+    raise ValueError("Config file must have [MongoDB] section with url value")
+if not utils.check_if_exists(appconfig, "Flask", "secret_key"):
+    raise ValueError("Config file must have [Flask] section with secret_key value")
 
 app=Flask(__name__)
 app.config["DEBUG"]=True
 app.config["MONGO_URI"]=appconfig["MongoDB"]["url"]
 
-app.config["SECRET_KEY"]=utils.read_if_exists(appconfig,"Flask","secret_key",os.urandom(32))
+app.config["SECRET_KEY"]=appconfig["Flask"]["secret_key"]
 # 1hr default
 app.config["token_timeout"]=int(utils.read_if_exists(appconfig,"secure_notes","token_timeout",3600))
 
